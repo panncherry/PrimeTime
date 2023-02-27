893c12d761418b4635aadec05f23244922631ca8
Pann Cherry <maung.eainmatmay@gmail.com>
2023-02-27 00:03:39 -0500

    Initial Commit

---
+++ PrimeTime/PrimeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeTimeApp.swift
+//  PrimeTime
+//
+//  Created by Pann Cherry on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PrimeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
