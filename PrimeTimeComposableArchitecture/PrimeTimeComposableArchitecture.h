628500ca76b7f2e6e72c2ff20dd0c013ebc1e35c
Pann Cherry <maung.eainmatmay@gmail.com>
2023-02-28 16:48:58 -0500

    Add PrimeTimeComposableArchitecture

---
+++ PrimeTimeComposableArchitecture/PrimeTimeComposableArchitecture.h
@@ -0,0 +1,18 @@
+//
+//  PrimeTimeComposableArchitecture.h
+//  PrimeTimeComposableArchitecture
+//
+//  Created by Pann Cherry on 2/28/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrimeTimeComposableArchitecture.
+FOUNDATION_EXPORT double PrimeTimeComposableArchitectureVersionNumber;
+
+//! Project version string for PrimeTimeComposableArchitecture.
+FOUNDATION_EXPORT const unsigned char PrimeTimeComposableArchitectureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrimeTimeComposableArchitecture/PublicHeader.h>
+
+
